1c69a8e8ea56687ad12b10df1f22a3b56a6116d6
Erik Johnson <erik.johnson@jogle.us>
2018-12-11 14:31:26 -0800

    Adding Day 11's solution.

---
+++ 2018/advent11.tcl
@@ -0,0 +1,78 @@
+#!/bin/sh
+# -*- tcl -*-
+# The next line is executed by /bin/sh, but not tcl \
+exec tclsh "$0" ${1+"$@"}
+
+package require math
+package require struct::matrix
+namespace import ::tcl::mathop::*
+
+# Get input from file
+set fhandle [open "temp.txt"]
+#set fhandle [open "advent11.txt"]
+set input [read -nonewline $fhandle]
+close $fhandle
+
+# Day 11: Chronal Charge
+#
+# Given a seed, compute a complex iterative function over a 300x300 array.
+#
+
+proc subtotal {x y size} {
+    return [expr { $::sapA($x,$y) - $::sapA([+ $x $size],$y)
+                 - $::sapA($x,[+ $y 1]) + $::sapA([+ $x 1],[+ $y 1]) }]
+}
+
+set input 1309
+for {set x 0} {$x <= 300} {incr x} {set sapA($x,0) 0}
+for {set y 1} {$y <= 300} {incr y} {
+    set sapA(0,$y) 0
+    for {set x 1} {$x <= 300} {incr x} {
+        set power [expr {(($x + 10) * $y) + $input}]
+        set power [expr {($power * ($x + 10))}]
+        set power [expr {int(($power % 1000) / 100) - 5}]
+        lappend rowL $power
+        set sapA($x,$y) [expr {$power + $sapA([- $x 1],$y)
+                                + $sapA($x,[- $y 1])
+                                - $sapA([- $x 1],[- $y 1]) }]
+    }
+}
+puts "Matrix built."
+
+# Part 1:
+# Find the UL corner of the 3x3 subgrid with the highest total power
+
+# Part 2:
+# Find the UL corner of the subgrid of ANY size with the highest total power.
+
+set max3Tot 0
+set maxTot 0
+for {set y 1} {$y <= 300} {incr y} {
+    for {set x 1} {$x <= 300} {incr x} {
+        set maxsize [expr {300 - ($x < $y ? $y : $x)}]
+        set stL {0}
+        for {set size 1} {$size < $maxsize} {incr size} {
+            set subtot [expr {  $::sapA($x,$y)
+                              + $::sapA([+ $x $size],[+ $y $size])
+                              - $::sapA([+ $x $size],$y)
+                              - $::sapA($x,[+ $y $size]) }]
+            if {($size == 3) && ($max3Tot < $subtot)} {
+                set max3Tot $subtot
+                set max3X [+ $x 1]
+                set max3Y [+ $y 1]
+            }
+            lappend stL $subtot
+        }
+        if {$maxTot < [::math::max {*}$stL]} {
+            set maxTot [::math::max {*}$stL]
+            set maxX [+ $x 1]
+            set maxY [+ $y 1]
+            set maxSize [lsearch $stL $maxTot]]
+        }
+    }
+    puts -nonewline "." ; flush stdout
+}
+
+
+puts "\nThe corner of the highest 3x3 subtotal ($max3Tot) is $max3X,$max3Y"
+puts "The corner of the highest any size subtotal ($maxTot) is $maxX,$maxY,$maxSize"
